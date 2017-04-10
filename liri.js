4160a5819735f3470ec9c635e296f60b0f493140
JuanOrozcoIt <Juan.Orozco.it@gmail.com>
2017-04-10 15:11:42 -0700

    dividing code sections with comments

--- liri.js
+++ liri.js
@@ -1,16 +1,4 @@
-//====================
-// At the top of the liri.js file, write the code you need to grab the data from keys.js. Then store the keys in a variable.
-// Make it so liri.js can take in one of the following commands:
-// my-tweets
-// spotify-this-song
-// movie-this
-// do-what-it-says
-//====================
-
-
-//====================
-//Global Variables
-//====================
+//All global variables
 var fs = require('fs');
 var keys = require('./keys.js');
 var Twitter = require('twitter');
@@ -18,9 +6,7 @@ var spotify = require('spotify');
 var request = require('request');
 var userInput = process.argv[2];
 var value = process.argv[3];
-//====================
-//Commands
-//====================
+// Section for commands
 function checkUserInput(userInput, value) {
     switch (userInput) {
         case "my-tweets":
@@ -37,16 +23,13 @@ function checkUserInput(userInput, value) {
             break;
     };
 };
-//====================
-//Twitter
-//====================
+// Section for twitter
 var client = new Twitter({
     consumer_key: keys.twitterKeys.consumer_key,
     consumer_secret: keys.twitterKeys.consumer_secret,
     access_token_key: keys.twitterKeys.access_token_key,
     access_token_secret: keys.twitterKeys.access_token_secret,
 });
-
 function twitter() {
     var params = { screen_name: '@nicolepolakk', count: '20' };
     client.get('statuses/user_timeline', params, function(error, tweets, response) {
@@ -54,12 +37,9 @@ function twitter() {
             console.log(tweets);
         };
     });
-
 };
 checkUserInput(userInput, value);
-//====================
-//Spotify
-//====================
+// Section for spotify
 function spotifyThis(songs) {
     spotify.search({ type: 'track', query: songs }, function(err, data) {
         if (err) {
@@ -83,9 +63,7 @@ function spotifyThis(songs) {
         };
     });
 };
-//====================
-//Movie
-//====================
+// Section for movie piece
 function movie(whatMovie) {
     request('http://www.omdbapi.com/?t=' + whatMovie + '&y=&plot=short&r=json', function(error, response, body) {
         if (!error && response.statusCode == 200) {
@@ -111,11 +89,8 @@ function movie(whatMovie) {
             saveToDocument("Rotten Tomatoes URL: " + json.tomatoURL);
         };
     });
-
 };
-//====================
-//whatItSays
-//====================
+// Section for whatItSays piece
 function whatItSays() {
     fs.readFile("random.txt", "utf-8", function read(err, data) {
         if (err) {
@@ -126,16 +101,11 @@ function whatItSays() {
             value = myOwnProcessArgv[1];
             userInput = myOwnProcessArgv[0];
         };
-
         checkUserInput(userInput, value);
-
     });
 };
-//====================
-//Save to document
-//====================
+// Function to save to file
 function saveToDocument(string){
-	
 	fs.appendFile('output.txt', string, function(err) {
 		  if ( err ) {
 		        console.log('Error occurred: ' + err);
